2db541374b0e01daa5ab0dc6978cc8b2a6cf3e7f
rekebuba <abukiabdullahi1@gmail.com>
2024-03-28 23:58:55 +0300

    binary_trees

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -1,7 +1,7 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_nodes - A function that counts the nodes 
+ * binary_tree_nodes - A function that counts the nodes
  * with at least 1 child in a binary tree
  *
  * @tree: pointer to the root node of the tree to measure the size
@@ -9,7 +9,7 @@
  */
 size_t binary_tree_nodes(const binary_tree_t *tree)
 {
-    if (tree == NULL)
+	if (tree == NULL)
 	{
 		return (0);
 	}
