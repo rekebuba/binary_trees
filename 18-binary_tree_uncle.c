f001f2ac682c37f37994d8dce616a12d351f795f
rekebuba <abukiabdullahi1@gmail.com>
2024-07-17 22:18:16 +0300

    binary tree uncle

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -10,7 +10,7 @@ binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
 	binary_tree_t *result;
 
-	if (!node || node->parent || node->parent->parent)
+	if (!node || !node->parent || !node->parent->parent)
 	{
 		return (NULL);
 	}
