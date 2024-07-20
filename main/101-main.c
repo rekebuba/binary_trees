eec937f08d281fb8e2c9406fc2eec9c7a1bff02d
rekebuba <abukiabdullahi1@gmail.com>
2024-07-20 18:51:54 +0300

    binary tree levelorder

--- main/101-main.c
+++ main/101-main.c
@@ -7,7 +7,7 @@
  */
 void print_num(int n)
 {
-    printf("%d\n", n);
+	printf("%d\n", n);
 }
 
 /**
@@ -17,18 +17,27 @@ void print_num(int n)
  */
 int main(void)
 {
-    binary_tree_t *root;
+	binary_tree_t *root;
 
-    root = binary_tree_node(NULL, 98);
-    root->left = binary_tree_node(root, 12);
-    root->right = binary_tree_node(root, 402);
-    root->left->left = binary_tree_node(root->left, 6);
-    root->left->right = binary_tree_node(root->left, 56);
-    root->right->left = binary_tree_node(root->right, 256);
-    root->right->right = binary_tree_node(root->right, 512);
+	root = binary_tree_node(NULL, 1);
+	root->left = binary_tree_node(root, 2);
+	root->right = binary_tree_node(root, 3);
+	root->left->left = binary_tree_node(root->left, 4);
+	root->left->right = binary_tree_node(root->left, 5);
+	root->right->left = binary_tree_node(root->right, 6);
+	root->right->right = binary_tree_node(root->right, 7);
 
-    binary_tree_print(root);
-    binary_tree_levelorder(root, &print_num);
-    binary_tree_delete(root);
-    return (0);
+	root->left->left->left = binary_tree_node(root->left->left, 8);
+	root->left->left->right = binary_tree_node(root->left->left, 9);
+	root->left->right->left = binary_tree_node(root->left->right, 10);
+	root->left->right->right = binary_tree_node(root->left->right, 11);
+	root->right->left->left = binary_tree_node(root->right->left, 12);
+	root->right->left->right = binary_tree_node(root->right->left, 13);
+	root->right->right->left = binary_tree_node(root->right->right, 14);
+	root->right->right->right = binary_tree_node(root->right->right, 15);
+
+	binary_tree_print(root);
+	binary_tree_levelorder(root, &print_num);
+	binary_tree_delete(root);
+	return (0);
 }
